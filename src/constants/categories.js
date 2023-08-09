5de39b79f569d94217b3327246c929c2ef36ded9
MUHAMMAD FARMAN <98874196+MUHAMMAD-FARMAN@users.noreply.github.com>
2023-08-09 20:45:11 +0500

    version 1

---
+++ src/constants/categories.js
@@ -0,0 +1,34 @@
+const CATEGORIES = [
+  {
+    key: '0',
+    text: 'Any Category',
+    value: '0'
+  },
+  {
+    key: 9,
+    text: 'General Knowledge',
+    value: 9
+  },
+  {
+    key: 17,
+    text: 'Science & Nature',
+    value: 17
+  },
+  {
+    key: 18,
+    text: 'Science: Computers',
+    value: 18
+  },
+  {
+    key: 19,
+    text: 'Science: Mathematics',
+    value: 19
+  },
+  {
+    key: 30,
+    text: 'Science: Gadgets',
+    value: 30
+  } 
+];
+
+export default CATEGORIES;
